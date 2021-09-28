135505d81ddd8802047b2eb3fbfe1e89748beb6d
Nur Hibnasitar <87582881+Hibnastiar@users.noreply.github.com>
2021-09-28 18:30:22 +0700

    Add files via upload

---
+++ Variable.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	
+	cout << "======INTEGER====== \n";
+	cout << "x = 20 \n";
+	int x;
+	x=20;
+	
+	cout << "y = x-7 \n";
+	int y;
+	y=x-7;
+	
+	cout << "Jumlah dari x+y ";
+	cout << x+y << endl ;
+	
+	cout << "======FLOAT====== \n";
+	cout << "p = 5.4965789 \n";
+	float p;
+	p=5.4965789;
+	
+	cout << "q = 4.2956789 \n";
+	float q;
+	q=4.2956789;
+	
+	cout << "Jumlah dari p+q ";
+	cout << p+q << endl ;
+	
+	return 0;
+}
